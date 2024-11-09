ec4f16d35479eab803cea497c395bdcba59ccec2
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2024-11-09 13:38:25 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2024, Sergiu Deitsch'
 author = 'Sergiu Deitsch'
 
 version = '0.1'
-release = '0.1.0a2'
+release = '0.1.0b1'
 
 # -- General configuration ---------------------------------------------------
 
