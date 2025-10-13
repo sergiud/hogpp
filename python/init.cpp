45ac5d1622e3243ee39d786c00aed2ccd11ddc08
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2025-10-13 17:53:23 +0200

    wip

--- python/init.cpp
+++ python/init.cpp
@@ -17,12 +17,412 @@
 // limitations under the License.
 //
 
+#include <cstdint>
+#include <cstdlib>
+#include <string_view>
+
 #include <pybind11/pybind11.h>
 
-void init_hogpp_x86_64(pybind11::module& m);
-void init_hogpp_x86_64_v2(pybind11::module& m);
-void init_hogpp_x86_64_v3(pybind11::module& m);
-void init_hogpp_x86_64_v4(pybind11::module& m);
+#include <fmt/format.h>
+
+#include "isa.hpp"
+
+enum class ISA : std::uint8_t
+{
+    Default,
+    SSE2,
+    SSE3,
+    SSSE3,
+    SSE4_1,
+    SSE4_2,
+    AVX,
+    AVX2,
+    AVX512,
+    AVX10_1,
+    AVX10_2,
+};
+
+template<ISA Type>
+struct CPUFeature;
+
+template<ISA Type>
+struct HOGppModule
+{
+    static void initialize(pybind11::module& m)
+    {
+        void init_hogpp_default(pybind11::module & m);
+        init_hogpp_default(m);
+    }
+};
+
+template<>
+struct HOGppModule<ISA::SSE2>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_SSE2)
+        void init_hogpp_sse2(pybind11::module & m);
+        init_hogpp_sse2(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::SSE2>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "SSE2";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_SSE2)
+        return __builtin_cpu_supports("sse2");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::SSE3>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_SSE3)
+        void init_hogpp_sse3(pybind11::module & m);
+        init_hogpp_sse3(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::SSE3>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "SSE3";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_SSE3)
+        return __builtin_cpu_supports("sse3");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::SSSE3>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_SSSE3)
+        void init_hogpp_ssse3(pybind11::module & m);
+        init_hogpp_ssse3(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::SSSE3>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "SSSE3";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_SSSE3)
+        return __builtin_cpu_supports("ssse3");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::SSE4_1>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_SSE4_1)
+        void init_hogpp_sse4_1(pybind11::module & m);
+        init_hogpp_sse4_1(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::SSE4_1>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "SSE4.1";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_SSE4_1)
+        return __builtin_cpu_supports("sse4.1");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::SSE4_2>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_SSE4_2)
+        void init_hogpp_sse4_2(pybind11::module & m);
+        init_hogpp_sse4_2(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::SSE4_2>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "SSE4.2";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_SSE4_2)
+        return __builtin_cpu_supports("sse4.2");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::AVX>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_AVX2)
+        void init_hogpp_avx(pybind11::module & m);
+        init_hogpp_avx(m);
+#else
+        // TODO Throw
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::AVX>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "AVX";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_AVX2)
+        return __builtin_cpu_supports("avx");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::AVX2>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_AVX2)
+        void init_hogpp_avx2(pybind11::module & m);
+        init_hogpp_avx2(m);
+#else
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::AVX2>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "AVX2";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_AVX2)
+        return __builtin_cpu_supports("avx2");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::AVX512>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_AVX512F)
+        void init_hogpp_avx512f(pybind11::module & m);
+        init_hogpp_avx512f(m);
+#else
+        // TODO Trhow
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::AVX512>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "AVX512";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_AVX512F)
+        return __builtin_cpu_supports("avx512f");
+#else
+        return false;
+#endif
+    }
+};
+
+template<>
+struct HOGppModule<ISA::AVX10_1>
+{
+    static void initialize(pybind11::module& m)
+    {
+#if defined(HAVE_ISA_AVX10_1)
+        void init_hogpp_avx10_1(pybind11::module & m);
+        init_hogpp_avx10_1(m);
+#else
+        // TODO Throw
+#endif
+    }
+};
+
+template<>
+struct CPUFeature<ISA::AVX10_1>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "AVX10.1";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+#if defined(HAVE_ISA_AVX10_1)
+        return __builtin_cpu_supports("avx10.1");
+#else
+        return false;
+#endif
+    }
+};
+
+#if defined(HAVE_ISA_AVX10_2)
+
+template<>
+struct HOGppModule<ISA::AVX10_2>
+{
+    static void initialize(pybind11::module& m)
+    {
+        void init_hogpp_avx10_2(pybind11::module & m);
+        init_hogpp_avx10_2(m);
+    }
+};
+
+template<>
+struct CPUFeature<ISA::AVX10_2>
+{
+    [[nodiscard]] constexpr static std::string_view name() noexcept
+    {
+        return "AVX10.2";
+    }
+
+    [[nodiscard]] static bool supported() noexcept
+    {
+        return __builtin_cpu_supports("avx10.2");
+    }
+};
+#endif
+
+template<ISA... Types>
+struct CPUFeatures
+{
+};
+
+using AvailableCPUFeatures = CPUFeatures
+    // clang-format off
+<
+      ISA::AVX10_2
+    , ISA::AVX10_1
+    , ISA::AVX512
+    , ISA::AVX2
+    , ISA::SSE4_2
+    , ISA::SSE4_1
+    , ISA::SSSE3
+    , ISA::SSE2
+>;
+// clang-format on
+
+namespace {
+
+void init_hogpp(pybind11::module& m, std::string_view isa,
+                CPUFeatures<> /*unused*/)
+{
+    if (!isa.empty()) {
+        using namespace fmt::literals;
+
+        throw pybind11::import_error{fmt::format(
+            FMT_STRING("ISA specified by the HOGPP_DISPATCH environment "
+                       "variable (\"{isa}\") is not available"),
+            "isa"_a = isa)};
+    }
+
+    HOGppModule<ISA::Default>::initialize(m);
+}
+
+template<ISA Type, ISA... Types>
+void init_hogpp(pybind11::module& m, std::string_view isa,
+                CPUFeatures<Type, Types...> /*unused*/)
+{
+    if (isa.empty() && CPUFeature<Type>::supported()) {
+        HOGppModule<Type>::initialize(m);
+    }
+    else if (isa == CPUFeature<Type>::name() &&
+             !CPUFeature<Type>::supported()) {
+        using namespace fmt::literals;
+
+        throw pybind11::import_error{fmt::format(
+            FMT_STRING("ISA specified by the HOGPP_DISPATCH environment "
+                       "variable (\"{isa}\") is available but not "
+                       "supported by the CPU"),
+            "isa"_a = isa)};
+    }
+    else {
+        init_hogpp(m, isa, CPUFeatures<Types...>{});
+    }
+}
+
+} // namespace
 
 #if defined(HOGPP_GIL_DISABLED)
 #    define HOGPP_MODULE(name, module, ...) \
@@ -39,16 +439,11 @@ void init_hogpp_x86_64_v4(pybind11::module& m);
 
 HOGPP_MODULE(HOGPP_MODULE_NAME, m)
 {
-    if (__builtin_cpu_supports("x86-64-v4")) {
-        init_hogpp_x86_64_v4(m);
-    }
-    else if (__builtin_cpu_supports("x86-64-v3")) {
-        init_hogpp_x86_64_v3(m);
-    }
-    else if (__builtin_cpu_supports("x86-64-v2")) {
-        init_hogpp_x86_64_v2(m);
-    }
-    else {
-        init_hogpp_x86_64(m);
-    }
+    const char* const isa = std::getenv("HOGPP_DISPATCH");
+    // TODO Log detected dispatch
+    // TODO Allow to override the dispatch using HOGPP_DISPATCH environment
+    // variable
+    //
+
+    init_hogpp(m, isa, AvailableCPUFeatures{});
 }
